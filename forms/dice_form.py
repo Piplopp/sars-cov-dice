8cb65002a7e9863d72d4ef42d58e5c6f083b0067
Piplopp <12142931+Piplopp@users.noreply.github.com>
2024-05-08 23:47:21 +0200

    add client side input validation messages

--- forms/dice_form.py
+++ forms/dice_form.py
@@ -1,13 +1,17 @@
 from wtforms import Form, IntegerField, validators
 from config import MAX_DIE_ALLOWED, MAX_REPEATS_ALLOWED
 
+RENDER_OPT = {'onblur': 'validateAllInputs()'}  # validateInput is a JS function called when leaving focus
+DICE_VALIDATOR = validators.NumberRange(0, MAX_DIE_ALLOWED)
+REPEATS_VALIDATOR = validators.NumberRange(1, MAX_REPEATS_ALLOWED)
+
 class DiceForm(Form):
-    d2 = IntegerField('d2', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d4 = IntegerField('d4', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d6 = IntegerField('d6', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d8 = IntegerField('d8', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d10 = IntegerField('d10', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d12 = IntegerField('d12', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d20 = IntegerField('d20', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    d100 = IntegerField('d100', validators=[validators.NumberRange(0, MAX_DIE_ALLOWED)], default=0)
-    n_repeats = IntegerField('n_repeats', validators=[validators.NumberRange(1, MAX_REPEATS_ALLOWED)], default=1)
+    d2 = IntegerField('d2', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d4 = IntegerField('d4', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d6 = IntegerField('d6', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d8 = IntegerField('d8', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d10 = IntegerField('d10', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d12 = IntegerField('d12', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d20 = IntegerField('d20', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    d100 = IntegerField('d100', default=0, validators=[DICE_VALIDATOR], render_kw=RENDER_OPT)
+    n_repeats = IntegerField('n_repeats', default=1, validators=[REPEATS_VALIDATOR], render_kw=RENDER_OPT)
--- static/scripts.js
+++ static/scripts.js
@@ -3,13 +3,81 @@ function rollDice() {
 
     fetch('/roll_dice', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(Object.fromEntries(formData))
+        body: formData
     })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        .then(response => response.text())
+        .then(html => {
+            document.documentElement.innerHTML = html;
             // Handle response data (if any)
         })
         .catch(error => console.error('Error:', error));
+}
+
+
+
+
+
+function validateInput(inputField, errorContainer, inputFieldType) {
+    var value = inputField.value.trim();
+
+    // Let's remove existing error messages just in case
+    if (errorContainer && errorContainer.classList.contains('invalid-input')) {
+        errorContainer.innerHTML = '';
+    } else {
+        // Create the empty errorContainer if it does not exists
+        errorContainer = document.createElement('div');
+        errorContainer.className = 'invalid-input';
+        inputField.parentNode.insertBefore(errorContainer, inputField.nextSibling);
+    }
+
+    // Now let's really validate stuff
+    if (value !== '') {
+        if (inputFieldType === 'diceInput') {
+            if (!Number.isInteger(Number(value)) || Number(value) < 0) {
+                // Change input field style
+                inputField.style.borderColor = 'red';
+                inputField.style.color = 'red';
+
+                // Create error message
+                var errorMessageElement = document.createElement('span');
+                errorMessageElement.textContent = 'Must be an integer >= 0.';
+                errorContainer.appendChild(errorMessageElement);
+                return;
+            }
+        } else if (inputFieldType === 'repeatsInput') {
+            if (!Number.isInteger(Number(value)) || Number(value) < 0) {
+                // Change input field style
+                inputField.style.borderColor = 'red';
+                inputField.style.color = 'red';
+
+                // Create error message
+                var errorMessageElement = document.createElement('span');
+                errorMessageElement.textContent = 'Must be an integer > 0';
+                errorContainer.appendChild(errorMessageElement);
+                return;
+            }
+        }
+    }
+
+    // Otherwise if input is valid, just reset the style
+    inputField.style.borderColor = '';
+    inputField.style.color = '';
+}
+
+function validateDiceInputs(){
+    var diceInputs = document.querySelectorAll('.dice-inputs input');
+    diceInputs.forEach(function(x) {
+        validateInput(x, x.nextElementSibling, 'diceInput')
+    });
+}
+
+function validateRepeatsInput(){
+    var repeatsInput = document.getElementById('n_repeats');
+    validateInput(repeatsInput, repeatsInput.nextElementSibling, 'repeatsInput')
+}
+
+
+function validateAllInputs() {
+    validateDiceInputs();
+    validateRepeatsInput();
 }
