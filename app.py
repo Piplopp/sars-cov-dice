d966de4aba51b685565a7fa49dbde7fec3dbff8c
Piplopp <12142931+Piplopp@users.noreply.github.com>
2024-05-09 00:04:44 +0200

    basic route do nothing except validate backend side form data

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
 from flask import Flask, request, jsonify, render_template
-
+from forms.dice_form import DiceForm
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    dice_form = DiceForm()
+    return render_template('index.html', dice_form=dice_form)
 
 @app.route('/roll_dice', methods=['POST'])
 def roll_dice():
     # Get JSON data
-    data = request.json
-    num_repeats = int(data['num_repeats'])
-    d2 = int(data['d2'])
-    d4 = int(data['d4'])
-    d6 = int(data['d6'])
-    d8 = int(data['d8'])
-    d10 = int(data['d10'])
-    d12 = int(data['d12'])
-    d20 = int(data['d20'])
-    d100 = int(data['d100'])
-
-    # Process the data (e.g., perform dice rolls)
-    # Return the result as JSON
-    result = {
-        'num_repeats': num_repeats,
-        'd2': d2,
-        'd4': d4,
-        'd6': d6,
-        'd8': d8,
-        'd10': d10,
-        'd12': d12,
-        'd20': d20,
-        'd100': d100
-    }
-    print(result)
-    return jsonify(result)
+    data = DiceForm(request.form)
+    data.validate()
+    
+    
+    return render_template('index.html', dice_form=data)
 
 
 
